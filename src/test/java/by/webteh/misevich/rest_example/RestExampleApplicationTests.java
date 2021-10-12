4d0c42646eda0660e88753cc6a8e0f8127f82c33
Мисевич Татьяна Евгеньевна <te.misevich@lab.vebtech.by>
2021-10-12 19:10:35 +0300

    Initial commit

---
+++ src/test/java/by/webteh/misevich/rest_example/RestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package by.webteh.misevich.rest_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
